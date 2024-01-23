28f5e01e1db2825f1578e12ac582f79abd5ba9a7
Sierraaa <143159653+asierra619@users.noreply.github.com>
2024-01-22 20:41:36 -0800

    added models and datatypes, added connection.js to config, and removed packagelock.json from gitignore

--- models/tea.js
+++
@@ -1,8 +0,0 @@
-// module.exports = (sequelize, DataTypes) => {
-//     return sequelize.define('Tea', {
-//         name: DataTypes.STRING,
-//         description: DataTypes.STRING,
-//         price: DataTypes.DECIMAL,
-//         imageUrl: DataTypes.STRING
-//     });
-// };
