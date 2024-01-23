a2c57c21a150eb76acabe2f9c8f0fa1e7d6ce47f
Sierraaa <143159653+asierra619@users.noreply.github.com>
2024-01-22 21:09:37 -0800

    added last models, will add relations later

--- models/OrderItem.js
+++ models/OrderItem.js
@@ -1,3 +1,44 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection.js');
 
+class OrderItem extends Model {}
+
+OrderItem.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        qty: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: false,
+            autoIncrement: false,
+        },
+        itemId: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'item',
+                key: 'id',
+            },
+        },
+        orderId: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'order',
+                key: 'id',
+            },
+        },
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'orderItem',
+    },
+);
+
+module.exports = OrderItem;
