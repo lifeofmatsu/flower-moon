424131c2fc8b8e511dd2534c1db405ad87a6a13e
Brandon <beegee54321@gmail.com>
2024-01-25 18:56:50 -0800

    linked new routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,11 @@
 const router = require('express').Router();
 const teaRoutes = require('./teaRoutes');
 const userRoutes = require('./userRoutes');
+const orderRoutes = require('./orderRoutes');
+const cartRoutes = require('./cartRoutes');
 
+router.use('/cart', cartRoutes);
+router.use('/orders', orderRoutes);
 router.use('/teas', teaRoutes);
 router.use('/users', userRoutes);
 
